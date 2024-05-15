67bbdd6b71ad51bca700009b743b1366e7ec1865
Ajitesh Nair <ajitesh4@illinois.edu>
2024-05-15 13:12:02 -0500

    test

--- src/portfolio.js
+++ src/portfolio.js
@@ -23,7 +23,7 @@ const greeting = {
   username: "Ajitesh Nair",
   title: "Hi all, I'm Ajitesh",
   subTitle: emoji(
-    "Work in Progress... Do not click on any links. A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "Work in Progress.... Do not click on any links. A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
   resumeLink:
     "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
